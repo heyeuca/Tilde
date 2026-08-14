3eeb68b046ec60e0a95c5fe10d55b10825e2dff4
weensy <weensy@euca.co>
2026-08-14 15:14:32 +0900

    Init

---
+++ Tilde/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tilde
+//
+//  Created by Weensy on 2026/08/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tilde/TildeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TildeApp.swift
+//  Tilde
+//
+//  Created by Weensy on 2026/08/13.
+//
+
+import SwiftUI
+
+@main
+struct TildeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
